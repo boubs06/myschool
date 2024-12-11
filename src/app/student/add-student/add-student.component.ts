67212f77cb9ba2d6a14ac58cae0c9774539bcdb4
boubs06 <pbdiallo11@gmail.com>
2024-12-11 17:17:54 +0000

    Add step add a student

--- src/app/student/add-student/add-student.component.ts
+++ src/app/student/add-student/add-student.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, UntypedFormBuilder, Validators } from '@angular/forms';
 import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
 
 @Component({
   selector: 'app-add-student',
   standalone: true,
-  imports: [MatFormField],
+  imports: [MatFormField, FormsModule,ReactiveFormsModule,MatFormFieldModule,MatInputModule],
   templateUrl: './add-student.component.html',
   styleUrl: './add-student.component.css'
 })
-export class AddStudentComponent {
+export class AddStudentComponent implements OnInit{
+  form : FormGroup
+  
+  constructor(private bottomSheetRef: MatBottomSheetRef<AddStudentComponent>) {}
 
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      numero: new FormControl('',Validators.required),
+      prenom: new FormControl('',Validators.required),
+      nom: new FormControl('',Validators.required),
+      classe: new FormControl('',Validators.required),
+  })
+  }
+
+  enregistrer() {
+    console.log("saved ascendant  >>>>>>>>>>>>>>>>>>>>>>>>>>>>", this.form.value)
+
+    
+      this.bottomSheetRef.dismiss({ result: this.form.value });
+ 
+    
+  }
 }
